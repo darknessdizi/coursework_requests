5e051899b85395df57f857ed2e6a19c421171fab
Remezov Dmitriy <darknessdizi@gmail.com>
2022-09-30 22:11:44 +0700

    add input_id_and_token

--- coursework_requests.py
+++ coursework_requests.py
@@ -7,10 +7,10 @@ class TokenForApi:
 
     URL = 'https://api.vk.com/method/'
 
-    def __init__(self, version='5.194'):
-        with open('token_vk.txt') as file, open('token_yandex.txt') as f:
+    def __init__(self, token, version='5.194'):
+        with open('token_vk.txt') as file:
             self.token_vk = file.readline()
-            self.token_yandex = f.readline()
+        self.token_yandex = token
 
         self.params_vk = {
             'access_token': self.token_vk,
@@ -34,11 +34,13 @@ class TokenForApi:
         while True:
             response = requests.get(url, params={
                 **self.params_vk, **parameters}).json()
+
             time.sleep(0.4)
             if 'error' in response:
                 print(
                     '\nВнимание!!! Ошибка запроса: ', 
                     response['error']['error_msg'], end='\n\n')
+
             for i in response['response']['items']:
                 my_list.append({
                     'size': i['sizes'][-1]['type'], 
@@ -46,6 +48,7 @@ class TokenForApi:
                         'likes': i['likes']['count'],
                         'url': i['sizes'][-1]['url']}})
                 count += 1
+
             if count == 200:
                 parameters['offset'] += 200
                 count = 0
@@ -53,6 +56,9 @@ class TokenForApi:
                 break
         # pprint.pprint(my_list)
         return my_list
+
+    def _add_photo_to_list():
+        pass
 
     def save_photos_to_yandex(self, object, number=5):
 
@@ -68,13 +74,7 @@ class TokenForApi:
                 url, headers=self.headers_yandex, params=parameters).json()
             p = requests.get(element['photo']['url'])
             print(p)
-            # out = open(response['href'], "wb")
-            # out.write(p.content)
-            # out.close()
-            # files = {'files': open(element["photo"]["url"], 'rb')}
             response = requests.put(
-                # response['href'], files=p.content,
-                # files = {'media': open('test.jpg', 'rb')}
                 response['href'], files={'file': p.content}, 
                 headers=self.headers_yandex, params=parameters)
 
@@ -86,13 +86,23 @@ class TokenForApi:
         name_folders = list(time.gmtime()[0:3])[::-1]
         name_folders = '_'.join(list(map(str, name_folders)))
         parameters = {'path': name_folders, 'overwrite': 'false'}
-        response = requests.put(url, headers=self.headers_yandex, params=parameters)
+        requests.put(url, headers=self.headers_yandex, params=parameters)
         return name_folders
 
+def input_id_and_token():
+    id_user = input("Введите ID пользователя: ")
+    if id_user == '':
+        id_user = 33579332
+        # id_user = 1105788
+    token_yandex = input("Введите token с полигона Yandex: ")
+    if token_yandex == '':
+        with open('token_yandex.txt') as file:
+            token_yandex = file.readline()
+    return id_user, token_yandex
+
 if __name__ == '__main__':
-    id_user = 33579332
-    # id_user = 1105788
-    my_token = TokenForApi()
+    id_user, token_yandex = input_id_and_token()
+    my_token = TokenForApi(token_yandex)
     object = my_token.get_photos_to_vk(id_user)
     # pprint.pprint(object)
     my_token.save_photos_to_yandex(object)
