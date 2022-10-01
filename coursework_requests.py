21060243837e9e65e07e120e6f1526a97a04aa10
Remezov Dmitriy <darknessdizi@gmail.com>
2022-10-01 20:48:31 +0700

    добавлены коментарии к функциям и методам. добавлена проверка на одинаковые имена.

--- coursework_requests.py
+++ coursework_requests.py
@@ -5,13 +5,29 @@ import time
 
 class TokenForApi:
 
+    '''Класс для работы с Api VK и Api Yandex'''
+
     URL = 'https://api.vk.com/method/'
 
-    def __init__(self, token, version='5.194', number=5):
+    def __init__(self, token, number=5, version='5.194'):
+
+        '''Конструктор класса. На вход получает токен для работы с 
+        
+        Yandex, версию Api для VK (по умолчанию версия 5.194) и 
+
+        количество фотографий для загрузки (может принимать значения
+        
+        количество, строка 'all' или по умолчанию 5). Конструктор
+
+        считывает токен для Api VK из файла token_vk.txt.
+        
+        '''
+
         with open('token_vk.txt') as file:
             self.token_vk = file.readline()
         self.token_yandex = token
         self.number = number
+        self.final_list = []
 
         self.params_vk = {
             'access_token': self.token_vk,
@@ -22,6 +38,19 @@ class TokenForApi:
             'Authorization': f'OAuth {self.token_yandex}'}
 
     def get_photos_to_vk(self, id=None):
+
+        '''Метод на вход получает номер ID пользователя (по умолчанию
+        
+        None, текущий пользователь). Метод реализует get запрос на 
+        
+        Api VK для получения всех фотографий пользователя или указанного
+        
+        количества. Данные о фотографии (имя, размер, url и likes) 
+        
+        добавляются в словарь. Словари добавляются в поле final_list.
+        
+        '''
+
         url = TokenForApi.URL + 'photos.getAll?'
         parameters = {
             'owner_id': id ,
@@ -29,59 +58,88 @@ class TokenForApi:
             'offset': 0,
             'count': 200,
             'photo_sizes': 1}
+        list_name = []
 
         while True:
             response = requests.get(url, params={
                 **self.params_vk, **parameters}).json()
-
             time.sleep(0.4)
             if 'error' in response:
                 print(
-                    '\nВнимание!!! Ошибка запроса: ', 
+                    '\nВнимание!!! Ошибка запроса Api VK: ', 
                     response['error']['error_msg'], end='\n\n')
 
-            my_list = self._add_photo_to_list(response) 
+            list_name = self._add_photo_to_list(response, list_name) 
 
-            if len(my_list) == 200:
-                parameters['offset'] += 200
-            else:
+            if len(self.final_list) == response['response']['count']:
                 break
-        pprint.pprint(my_list)
-        return my_list
+            elif len(self.final_list) == self.number:
+                    break
+            elif len(self.final_list) % 200 == 0:
+                parameters['offset'] += 200
+        pprint.pprint(len(self.final_list))
 
-    def _add_photo_to_list(self, response):
-        my_list = []
-        count = 0
+    def _add_photo_to_list(self, response, list_name):
+
+        '''Метод на вход получает json объект из которого достается url
+        
+        фотографии, определяется максимальный размер, расширение файла,
+        
+        добавляется имя на основе likes фотографии. Проводится проверка
+        
+        на наличие одинаковых имен. Данные на фотографии объединяются 
+        
+        в словарь и добавляются в поле класса final_list. 
+
+        '''
+
         for i in response['response']['items']:
                 name_file = i['sizes'][-1]['url'].split('/')[-1].split('?')[0]
                 index = name_file.find('.')
                 name_file = name_file[index:]
                 name_file = f'{i["likes"]["count"]}{name_file}'
-
-                my_list.append({
-                    'size': i['sizes'][-1]['type'], 
-                    'file_name': name_file,
-                    'photo': {
-                        'likes': i['likes']['count'],
-                        'url': i['sizes'][-1]['url']}})
-                count += 1
-                
-                if self.number == 'all':
-                    pass
-                elif count == self.number:
+                while True:
+                    if name_file in list_name:
+                        name = name_file.find('(')
+                        if name == -1:
+                            name_file = name_file.split('.')
+                            name_file[0] += '(2)'
+                            name_file = '.'.join(name_file)
+                        else:
+                            name_file = name_file.split('.')
+                            count = int(name_file[0][(
+                                name_file[0].rfind('(')) + 1:-1]) + 1
+                            name_file[0] = str(i["likes"]["count"]) + f'({count})'
+                            name_file = '.'.join(name_file)
+                    else:
+                        self.final_list.append({
+                            'size': i['sizes'][-1]['type'], 
+                            'file_name': name_file,
+                            'photo': {
+                                'likes': i['likes']['count'],
+                                'url': i['sizes'][-1]['url']}})
+                        list_name.append(name_file)
+                        break
+    
+                if len(self.final_list) == self.number:
                     break
-        return my_list
 
     def save_photos_to_yandex(self, object_list):
+
+        '''Метод принимает список словарей с данными на фотографии.
+        
+        Осуществляет get и put запрос на Api Yandex. Загружает 
+        
+        фотографии по ссылкам на Yandex.
+        
+        '''
 
         url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         name_folder = self.create_folder()
 
         for element in object_list:
-            # name_file = element['photo']['url'].split('/')[-1]
-            # name_path = name_folder + '/' + name_file
             name_path = name_folder + '/' + element['file_name']
-            parameters = {'path': name_path, 'overwrite': 'true', 'templated': True}
+            parameters = {'path': name_path, 'overwrite': 'true'}
 
             response = requests.get(
                 url, headers=self.headers_yandex, params=parameters).json()
@@ -91,20 +149,35 @@ class TokenForApi:
 
     def create_folder(self):
 
-        '''Создаем папку на яндекс диск'''
+        '''Метод класса создает папку на Yandex диск с именем текущей
+        
+        даты.
+        
+        '''
 
         url = 'https://cloud-api.yandex.net/v1/disk/resources' 
         name_folders = list(time.gmtime()[0:3])[::-1]
-        name_folders = '_'.join(list(map(str, name_folders)))
+        name_folders = '.'.join(list(map(str, name_folders)))
         parameters = {'path': name_folders, 'overwrite': 'false'}
         requests.put(url, headers=self.headers_yandex, params=parameters)
         return name_folders
 
+
 def input_id_and_token():
+
+    '''Функция реализует пользовательский ввод номера ID профиля и 
+    
+    токена с полигона Yandex (при вводе пустого поля, токен считывается
+    
+    из файла token_yandex.txt.)
+
+    '''
+
     id_user = input("Введите ID пользователя: ")
     if id_user == '':
-        id_user = 33579332
+        id_user = None
         # id_user = 1105788
+        # id_user = 2726270
     token_yandex = input("Введите token с полигона Yandex: ")
     if token_yandex == '':
         with open('token_yandex.txt') as file:
@@ -113,8 +186,7 @@ def input_id_and_token():
 
 if __name__ == '__main__':
     id_user, token_yandex = input_id_and_token()
-    my_token = TokenForApi(token_yandex, number=3)
-    object = my_token.get_photos_to_vk(id_user)
-    # pprint.pprint(object)
-    my_token.save_photos_to_yandex(object)
+    object = TokenForApi(token_yandex, 'all')
+    object.get_photos_to_vk(id_user)
+    object.save_photos_to_yandex(object.final_list)
     
